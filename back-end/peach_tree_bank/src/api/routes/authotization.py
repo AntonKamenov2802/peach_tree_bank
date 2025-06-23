79b7c986a20fcb4acabd8a931fbda7905e7e21a5
AntonKamenov2802 <anton.kamenov2802@gmail.com>
2025-06-23 19:06:45 +0000

    Adds GET and PATCH request for specific transactions

--- back-end/peach_tree_bank/src/api/routes/authotization.py
+++ back-end/peach_tree_bank/src/api/routes/authotization.py
@@ -3,6 +3,7 @@ from uuid import UUID
 from pydantic import BaseModel
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
+from sqlalchemy.exc import NoResultFound
 
 from database.utils import get_db_session
 from database.models import User
@@ -20,9 +21,14 @@ router = APIRouter(
 
 @router.post("/authenticate")
 def authenticate_with_username_and_password(user: User, session: Session = Depends(get_db_session)) -> AuthToken:
-    users = session.exec(select(User).where(User.username == user.username)).all()
-    if len(users) == 1 and users[0].password == user.password:
-        token = generate_authorization_token(users[0])
-        return AuthToken(token=token)
+    try:
+        user = session.exec(select(User).where(User.username == user.username)).one()
+        if user.password == user.password:
+            token = generate_authorization_token(user)
+            return AuthToken(token=token)
 
-    raise HTTPException(status_code=401, detail="Incorect username or password")
+        raise HTTPException(status_code=401, detail="Incorect username or password")
+
+    except NoResultFound:
+        raise HTTPException(status_code=401, detail="Incorect username or password")
+
--- back-end/peach_tree_bank/src/api/routes/transactions.py
+++ back-end/peach_tree_bank/src/api/routes/transactions.py
@@ -1,7 +1,11 @@
+from uuid import UUID
+
 from sqlmodel import Session, select
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.exc import NoResultFound
 
 from api.utils import is_authorized
+from api.models.models_transaction import TransactionPatch
 from database.utils import get_db_session
 from database.models import Transaction, User
 
@@ -15,3 +19,43 @@ router = APIRouter(
 def get_all_transactions(db_session: Session = Depends(get_db_session), user: User = Depends(is_authorized)) -> list[Transaction]:
     user_transactions = db_session.exec(select(Transaction).where(Transaction.user_id == user.id)).all()
     return user_transactions
+
+
+@router.get("/transaction/{transaction_id}")
+def get_transaction(transaction_id: UUID, db_session: Session = Depends(get_db_session), user: User = Depends(is_authorized)) -> Transaction:
+    return _get_transaction_from_db(transaction_id, db_session, user)
+
+
+
+@router.post("/transaction", status_code=201)
+def create_new_transaction(new_transaction: Transaction, db_session: Session = Depends(get_db_session), user: User = Depends(is_authorized)) -> Transaction:
+    new_transaction.user_id = user.id
+    db_session.add(new_transaction)
+    db_session.commit()
+    db_session.refresh(new_transaction)
+    return new_transaction
+
+
+@router.patch("/transaction/{transaction_id}")
+def patch_transaction(transaction_id: UUID, transaction_patch: TransactionPatch, db_session: Session = Depends(get_db_session), user: User = Depends(is_authorized)) -> TransactionPatch:
+    transaction = _get_transaction_from_db(transaction_id, db_session, user)
+
+    transaction.state = transaction_patch.state
+
+    db_session.add(transaction)
+    db_session.commit()
+    db_session.refresh(transaction)
+
+    return TransactionPatch(state=transaction.state)
+
+
+def _get_transaction_from_db(transaction_id: UUID, db_session: Session, user: User) -> Transaction:
+    try:
+        transaction = db_session.exec(select(Transaction).where(Transaction.id == transaction_id)).one()
+    except NoResultFound:
+        raise HTTPException(status_code=404, details="Transaction not fount.")
+
+    if transaction.user_id != user.id:
+        raise HTTPException(status_code=401, details="You do not have permission to view this transaction.")
+
+    return transaction
