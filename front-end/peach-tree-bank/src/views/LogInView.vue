38931ada8d4a413aa8326ae74650639cab52be94
AntonKamenov2802 <anton.kamenov2802@gmail.com>
2025-06-25 13:00:59 +0000

    Adds login page

---
+++ front-end/peach-tree-bank/src/views/LogInView.vue
@@ -0,0 +1,13 @@
+<template>
+    <form>
+        <input type="email" class="form-control" placeholder="name@example.com" required>
+        <input type="password" class="form-control" placeholder="Password" required>
+        <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'LogInView'
+}
+</script>
